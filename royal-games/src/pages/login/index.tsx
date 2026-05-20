88d92eb0a93d18a2a100d42490e3c7d3a49686c8
DIEGO CASTRO DA ROCHA <45255137804@info.local>
2026-05-20 10:58:12 -0300

    feat:loginFuncionando

--- royal-games/src/pages/login/index.tsx
+++ royal-games/src/pages/login/index.tsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import styles from "./login.module.css"
+import { login } from "../api/authService"
+import { ToastContainer, toast } from 'react-toastify';
 
-const login = () => {
+
+const Login = () => {
+
+    const [email, setEmail] = useState<string>("")
+    const [senha, setSenha] = useState<string>("")
+
+    const notificacao = (msg: string) => toast.success(msg);
+    const erro = (msg: string) => toast.error(msg);
+    
+
+    async function autenticar(e: React.FormEvent<HTMLFormElement>) {
+       e.preventDefault();
+       try{
+            await login(email, senha);
+            notificacao("Login bem sucedido")
+       }catch(error: any){
+            erro(error.message)
+        }
+    }
+
     return(
         <main id={styles.main}>
             <div id={styles.mulherLogin}>
@@ -8,16 +30,18 @@ const login = () => {
             </div>
             <div id={styles.formularioCompleto}>
                 <img src="./imgs/logologin.png" id={styles.logo} alt="" />
-                <form id={styles.form} action="">
+                <form id={styles.form} onSubmit={autenticar}>
                     <label>Email</label>
-                    <input type="email" id={styles.email}  required placeholder="email@gmail.com"/>
+                    <input type="email" id={styles.email}  required placeholder="email@gmail.com"
+                        value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <label>Senha</label>
-                    <input type="password" required placeholder="*****"/>
+                    <input type="password" required placeholder="*****"
+                        value={senha} onChange={(e) => setSenha(e.target.value)}/>
+                    <button>Entrar</button>
                 </form>
-                <button>Entrar</button>
             </div>
         </main>
     )
 }
 
-export default login
+export default Login
